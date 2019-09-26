b0826f85898c4f052487b288c79825f7c8f3d7d5
dyl trimble <dyltrimble12@gmail.com>
2019-09-25 23:24:19 -0400

    full function on bands in town

--- liri.js
+++ liri.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 var fs = require("fs")
 var keys = require("./keys.js");
 var axios = require("axios")
+var moment = require("moment")
 
 var Spotify = require('node-spotify-api');
 
@@ -19,17 +20,19 @@ let grabOMDB = function (movieName) {
     axios.get(urlHit).then(function (response) {
 
         var movieInfo = `
------- Liri found this for you -----
+    ------ Liri found this for you -----
 Title:        ${response.data.Title}
 Year:         ${response.data.year}
 Rated:        ${response.data.Rated}
 IMDB Raiting: ${response.data.imdbRating}
 Country:      ${response.data.Country}
 Language:     ${response.data.Language}
-Plot:         ${response.data.Plot}
+Plot:         
+${response.data.Plot}
 
 Actors:       ${response.data.Actors}
-IMDb Raiting: ${response.data.imdbRating}`;
+IMDb Raiting: ${response.data.imdbRating}
+    -------------------------------------`;
         console.log(movieInfo)
 
         fs.appendFile('log.txt', movieInfo, function (err) {
@@ -53,12 +56,12 @@ function grabSpotify(artName) {
             return
         } else {
             var spotInfo = `
-            ---------LIRI found this for you---------
-Song Name: ${songName.toUpperCase()}
-Album Name: ${data.tracks.items[0].album.name}
+    ---------LIRI found this for you---------
+Song Name:   ${songName.toUpperCase()}
+Album Name:  ${data.tracks.items[0].album.name}
 Artist Name: ${data.tracks.items[0].album.artists[0].name}
-URL: ${data.tracks.items[0].album.external_urls.spotify}
-     ----------------------------`
+URL:         ${data.tracks.items[0].album.external_urls.spotify}
+    -----------------------------------------`
                 ;
             console.log(spotInfo)
 
@@ -76,14 +79,25 @@ function grabBands(artName){
 
     let urlTarget = "https://rest.bandsintown.com/artists/" + artName + "/events?app_id=codingbootcamp";
     axios.get(urlTarget).then(function(response){
+        var date = moment(response.data[0].datetime).format('MM/DD/YYYY')
         var infoArtist = `
-Venue: ${response.data[0].venue}
-Location: ${response.data[0].venue.city}
-Date: ${response.datat[0].datetime}`;
+    ---------LIRI found this for you---------
+Venue:     ${response.data[0].venue.name}
+Location:  ${response.data[0].venue.city}, ${response.data[0].venue.country}
+Date:      ${date}
+    -----------------------------------------`;
 
         console.log(infoArtist);
+
+        fs.appendFile('log.txt', infoArtist, function (err) {
+            if (err) throw err
+            console.log('This information is Saved! in log.txt ')
+        });
+        
     });
 };
+   
+   
 
 
 doNext(userCommand, artName);
